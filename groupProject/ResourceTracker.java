53449f57adfec59c5b7aad4bd99c5b8789004b80
BloopTheSquid <maxorobinson@gmail.com>
2025-04-17 12:54:05 +0100

    setters and getters for ResourceTracker.java
    I'm helping so much surely totally for real

--- groupProject/ResourceTracker.java
+++ groupProject/ResourceTracker.java
@@ -3,4 +3,21 @@ package groupProject;
 public class ResourceTracker {
     String[] books;
     String[] lapEquip;
+    
+    //setters and getters
+    public String[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(String[] books) {
+        this.books = books;
+    }
+
+    public String[] getLabEquip() {
+        return labEquip;
+    }
+
+    public void setLabEquip(String[] labEquip) {
+        this.labEquip = labEquip;
+    }
 }
