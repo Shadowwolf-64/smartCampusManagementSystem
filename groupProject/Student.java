8920a207e5605e3ebeb0560453baf733b2a0425f
BloopTheSquid <maxorobinson@gmail.com>
2025-04-17 12:41:52 +0100

    Added setters and getters to Student.java
    double check the ones for lists I have no idea if it's right

--- groupProject/Student.java
+++ groupProject/Student.java
@@ -6,4 +6,45 @@ public class Student {
 	String degreePathway;
 	String[] enrolledModules;
 	int[] grades;
+	
+	//getters and setters
+	public String getStudentName() {
+		return name;
+	}
+
+	public void setStudentName() {
+		this.name = name;
+	}
+
+	public int getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+
+	public String getDegreePathway(String degreePathway) {
+		return degreePathway;
+	}
+
+	public void setDegreePathway() {
+		this.degreePathway = degreePathway;
+	}
+
+	public String[] getEnrolledModules() {
+		return enrolledModules;
+	}
+
+	public void setEnrolledModules(String[] enrolledModules) {
+		this.enrolledModules = enrolledModules;
+	}
+
+	public int[] getGrades() {
+		return grades;
+	}
+
+	public void setGrades(int grades) {
+		this.grades = new int[]{grades};
+	}
 }
