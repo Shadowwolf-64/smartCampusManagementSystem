3dfa22af32e45eb622c81215df28c77d938df425
SIeepIess <lewisburdekin@hotmail.com>
2025-04-17 14:55:53 +0100

    created TableModel class containing a custom table model

---
+++ groupProject/TableModel.java
@@ -0,0 +1,25 @@
+package groupProject;
+
+import javax.swing.table.DefaultTableModel;
+
+public class TableModel extends DefaultTableModel {
+    // Constructor with custom column names //
+    public TableModel(String[] columnNames) {
+        super(columnNames, 0);
+    }
+
+    //override method to set the data type for each column //
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        if (columnIndex == 3) {
+            return Boolean.class; // Sets the "Select" column to boolean
+        }
+        return String.class;
+    }
+
+    // Override method to make the checkbox column editable //
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        return column == 3; // Only the "Select" column is editable
+    }
+}
