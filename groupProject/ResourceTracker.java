4a75f7560a899a3bd34abbb10d42fffe51dcb49e
ebonwulf <ebonwolf00@gmail.com>
2025-04-22 09:05:09 +0100

    extra JLabels and JTextFields added to the ResourceTracker class and these labels and text fields added to the user input panel

--- groupProject/ResourceTracker.java
+++ groupProject/ResourceTracker.java
@@ -10,19 +10,23 @@ public class ResourceTracker extends JPanel{
     //variables
     private final List<Object> books = new ArrayList<>();
     private final List<Object> labEquipment = new ArrayList<>();
+    //JLabels for the student input fields in the userInputPanel
+    JLabel resourceName = new JLabel("Resource Name: ");
+    JLabel resourceID = new JLabel("Resource ID:");
+    JLabel availabilityLabel = new JLabel("Resource Availability: ");
+    JLabel departmentOrAuthorLabel = new JLabel("Department or Author:  ");
+
+    //JTextFields for the above JLabels for userInputPanel
+    JTextField name = new JTextField(10);
+    JTextField id = new JTextField(10);
+    JTextField availability = new JTextField(10);
+    JTextField departmentOrAuthor = new JTextField(10);
 
     public void resourceTracker(JPanel bookingInputCard, GridBagLayout layout, GridBagConstraints gbc) {
         //border and border title creation
         Border blackline = BorderFactory.createTitledBorder("Resources");
         //adds border and title to the card
         bookingInputCard.setBorder(blackline);
-        //JLabels for the student input fields in the userInputPanel
-        JLabel resourceName = new JLabel("Resource Name:  ");
-        JLabel resourceID = new JLabel("Resource ID:");
-
-        //JTextFields for the above JLabels for userInputPanel
-        JTextField name = new JTextField(10);
-        JTextField id = new JTextField(10);
 
         //adding the layout and constraints for the bookingInputCard
         bookingInputCard.setLayout(layout);
@@ -42,5 +46,21 @@ public class ResourceTracker extends JPanel{
         gbc.gridx = 1;
         gbc.gridy = 1;
         bookingInputCard.add(id, gbc);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        bookingInputCard.add(departmentOrAuthorLabel, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        bookingInputCard.add(departmentOrAuthor, gbc);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        bookingInputCard.add(availabilityLabel, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        bookingInputCard.add(availability, gbc);
     }
 }
